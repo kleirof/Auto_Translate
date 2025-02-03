2498194a4136b1fb5f5a2b8b9cd88d7c6b2985ac
kleirof <95400614+kleirof@users.noreply.github.com>
2025-02-03 17:44:27 +0800

    Add files via upload

---
+++ AutoTranslate/ITranslationService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections;
+
+namespace AutoTranslate
+{
+    public interface ITranslationService
+    {
+        IEnumerator StartTranslation(string[] texts, Action<string[]> callback);
+    }
+}
