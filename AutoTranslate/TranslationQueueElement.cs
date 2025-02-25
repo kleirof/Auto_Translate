d3c488271bba178002381c69494274291667587d
kleirof <95400614+kleirof@users.noreply.github.com>
2025-02-25 13:45:45 +0800

    Add files via upload

---
+++ AutoTranslate/TranslationQueueElement.cs
@@ -0,0 +1,14 @@
+﻿namespace AutoTranslate
+{
+    struct TranslationQueueElement
+    {
+        public string text;
+        public object control;
+
+        public TranslationQueueElement(string text, object control)
+        {
+            this.text = text;
+            this.control = control;
+        }
+    }
+}
